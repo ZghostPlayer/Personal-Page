d533b98717494bb4cc7b0a37584569b3d0a2ef27
@ZghostPlayer <@Zmanciofilho@gmail.com>
2024-04-24 16:05:11 -0300

    pre-release version

--- js/script.js
+++ js/script.js
@@ -44,5 +44,7 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
     // animation footer on scroll
+    let footer = document.querySelector('footer');
 
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
